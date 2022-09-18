6ddc7c93898ea6376d3db472ba45d1ff74d5fb6d
KillerDogs <kenybustamante2@hotmail.com>
2022-09-18 17:24:41 -0300

    fix error component Input

--- web/src/components/Form/Input.tsx
+++ web/src/components/Form/Input.tsx
@@ -2,10 +2,10 @@ import { InputHTMLAttributes } from 'react'
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
-export function Input(props:InputProps) {
+export function Input(props: InputProps) {
     return (
-        {...props},
         <input
+        {...props}
             className="bg-zinc-900 py-3 px-4 rounded text-sm placeholder:text-zinc-500"
         />
     )
