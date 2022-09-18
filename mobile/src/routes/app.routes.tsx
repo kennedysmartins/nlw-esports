d76e3b2635e0d04b3d6bde9e1f041d98359321c6
KillerDogs <kenybustamante2@hotmail.com>
2022-09-18 18:01:48 -0300

    Mobile configs

---
+++ mobile/src/routes/app.routes.tsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { Home } from '../screens/Home'
+import { Game } from '../screens/Game'
+
+const { Navigator, Screen } = createNativeStackNavigator();
+
+export function AppRoutes() {
+    return (
+        <Navigator>
+            <Screen
+                name="home"
+                components={Home}
+            />
+
+            <Screen
+                name="game"
+                components={Game}
+            />
+        </Navigator>
+    )
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,15 +1,26 @@
-import { View, Image, FlatList } from 'react-native';
+import {useEffect, useState } from 'react'
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard/indes';
+
+import { GameCard, GameCardProps } from '../../components/GameCard/indes';
 import { Heading } from '../../components/Heading';
 
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() =>{
+    fetch('http://192.168.0.36:3333/games')
+    .then( response => response.json())
+    .then( data => setGames(data))
+  },[])
+
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Image
         source={logoImg}
         style={styles.logo}
@@ -21,7 +32,7 @@ export function Home() {
       />
 
       <FlatList 
-        data={GAMES}
+        data={games}
         keyExtractor={item => item.id}
         renderItem={({ item }) => (
           <GameCard
@@ -33,6 +44,6 @@ export function Home() {
         contentContainerStyle = {styles.contentList}
       />
 
-    </View>
+    </SafeAreaView>
   );
 }
