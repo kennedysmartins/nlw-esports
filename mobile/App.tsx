00a4b95d54b0c7890131f6ff227d70d187ec45cb
KillerDogs <kenybustamante2@hotmail.com>
2022-09-18 01:13:33 -0300

    mobile-changes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,38 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'react-native';
 
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-      {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from './src/components/Background';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Button title="Botão 1"/>
-      <Button title="Botão 2"/>
-      <Button title="Botão 3"/>
-      <Button title="Botão 4"/>
+    <Background>
+      <StatusBar 
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+      />
 
-      <StatusBar style="auto" />
-    </View>
+    </Background>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#0000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
